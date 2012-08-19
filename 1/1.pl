ecd0915655289cec8a990a55895491dbfb7810bb
Ronald Kaiser <raios.catodicos@gmail.com>
2012-08-18 22:03:15 -0300

    refactoring perl solution

--- 1/1.pl
+++ 1/1.pl
@@ -1,4 +1,4 @@
 # File: 1.pl
 # Description: http://projecteuler.net/problem=1
 
-print eval join '+', grep { $_ % 3 == 0 || $_ % 5 == 0 } (3..999);
+print eval join '+', grep { !($_ % 3) || !($_ % 5) } (3..999);
