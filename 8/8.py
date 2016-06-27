70765654009606c8a9b2e7855d869f032ce9c8bf
Ronald Kaiser <raios.catodicos@gmail.com>
2016-06-26 23:26:28 -0300

    refactoring problem 8

--- 8/8.py
+++ 8/8.py
@@ -1,7 +1,8 @@
 from sys import stdin
+from operator import mul
 
 
 n = str(stdin.readline())
 
-print max([reduce(lambda x, y: int(x)*int(y), n[i:i+13])
+print max([reduce(mul, map(int, n[i:i+13]))
            for i in range(0, 987)])
